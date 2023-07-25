a93d8180184a4162d5a051e3df52ef242ada00c6
icheng <yicheng-me@qq.com>
2023-07-25 09:50:23 +0000

    feat: 动态路由的准备

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createRouter } from './create-router.tsx'
+import { RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+function render() {
+
+  const { router } = createRouter();
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App>
+        <RouterProvider router={router}  />
+      </App>
+    </React.StrictMode>,
+  )
+}
+
+render();
